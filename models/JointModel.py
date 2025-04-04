ecbdb51fe5472bef28855d3d5c6a542e21e03f0c
xabieralbizu <202207372@alu.comillas.edu>
2025-04-04 17:46:29 +0200

    Model updated

---
+++ models/JointModel.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from typing import Tuple
+
+class JointModel(torch.nn.Module):
+
+    def __init__(self, embedding_weights: torch.Tensor, hidden_dim: int, num_layers: int, num_ner_tags: int, num_sa_tags: int):
+
+        self.embedding: nn.Embedding = nn.Embedding.from_pretrained(embedding_weights, freeze=True)
+        self.lstm: nn.LSTM = nn.LSTM(embedding_weights.shape[1], hidden_size=hidden_dim, num_layers=num_layers, bidirectional=True)
+
+        self.ner_classifier: nn.Linear = nn.Linear(in_features=hidden_dim *2, out_features=num_ner_tags)
+        self.sa_classifier: nn.Linear = nn.Linear(in_features=hidden_dim *2, out_features=num_sa_tags)
+
+    def forward(self, inputs: torch.Tensor)->Tuple([torch.Tensor, torch.Tensor]): # type: ignore
+        #inputs has to contain word idxs
+        embedded_words = self.embedding(inputs)
+        lstm_out, (h, c) = self.lstm(embedded_words)
+
+        sa_logit = self.sa_classifier(torch.mean(h))
+        ner_logits = self.ner_classifier(lstm_out)
+
+        return ner_logits, sa_logit
